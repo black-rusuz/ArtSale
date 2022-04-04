6ac29862699e146736fa8525d73e9499b089a688
RUzai <RUzairov@gmail.com>
2022-04-04 19:56:52 +0300

    JDBC almost

--- src/main/java/ru/sfedu/artsale/api/DataProviderJdbc.java
+++ src/main/java/ru/sfedu/artsale/api/DataProviderJdbc.java
@@ -1,137 +1,274 @@
 package ru.sfedu.artsale.api;
 
+import ru.sfedu.artsale.Constants;
+import ru.sfedu.artsale.model.Result;
 import ru.sfedu.artsale.model.bean.*;
+import ru.sfedu.artsale.utils.ConfigurationUtil;
+import ru.sfedu.artsale.utils.JdbcUtil;
 
 import java.io.IOException;
+import java.sql.*;
+import java.util.ArrayList;
 import java.util.List;
 
 public class DataProviderJdbc extends AbstractDataProvider{
+    private final String hostname = ConfigurationUtil.getConfigurationEntry(Constants.H2_HOSTNAME);
+    private final String username = ConfigurationUtil.getConfigurationEntry(Constants.H2_USERNAME);
+    private final String password = ConfigurationUtil.getConfigurationEntry(Constants.H2_PASSWORD);
 
     protected DataProviderJdbc() throws IOException {
     }
 
+    private <T> List<T> read(Class<T> type) {
+        return read(type, JdbcUtil.selectAllFromTable(type.getSimpleName()));
+    }
+
+    private <T> List<T> read(Class<T> type, long id) {
+        return read(type, JdbcUtil.selectFromTableById(type.getSimpleName(), id));
+    }
+
+    private <T> List<T> read(Class<T> type, String sql) {
+        List<T> list = new ArrayList<>();
+        try {
+            Connection connection = DriverManager.getConnection(hostname, username, password);
+            Statement statement = connection.createStatement();
+            ResultSet resultSet = statement.executeQuery(sql);
+
+            log.debug(sql);
+            // list = getData(type, resultSet);
+
+            resultSet.close();
+            statement.close();
+            connection.close();
+        } catch (SQLException e) {
+            log.error(e.getMessage());
+        }
+        return list;
+    }
+
+    private void write(String sql) throws SQLException {
+        Connection connection = DriverManager.getConnection(hostname, username, password);
+        Statement statement = connection.createStatement();
+
+        log.debug(sql);
+        statement.executeUpdate(sql);
+
+        connection.close();
+        statement.close();
+    }
+
+    private <T> boolean write(String methodName, T bean, long id) {
+        String tableName = bean.getClass().getSimpleName();
+        String sql = switch (methodName) {
+            case Constants.METHOD_NAME_APPEND -> JdbcUtil.insertIntoTableValues(tableName, bean);
+            case Constants.METHOD_NAME_DELETE -> JdbcUtil.deleteFromTableById(tableName, id);
+            case Constants.METHOD_NAME_UPDATE -> JdbcUtil.updateTableSetById(tableName, bean, id);
+            default -> "";
+        };
+
+        try {
+            write(sql);
+        } catch (SQLException e) {
+            log.error(e.getMessage());
+            sendLogs(methodName, bean, Result.ERROR);
+            return false;
+        }
+        sendLogs(methodName, bean, Result.SUCCESS);
+        return true;
+    }
+
     @Override
     public List<User> getUsers() {
-        return null;
+        return read(User.class);
     }
 
     @Override
     public User getUser(long id) {
-        return null;
+        List<User> list = read(User.class, id);
+        return list.isEmpty() ? new User() : list.get(0);
     }
 
     @Override
     public long insertUser(User user) {
-        return 0;
+        long id = user.getId();
+        if (getUser(id).getId() != 0)
+            user.setId(System.currentTimeMillis());
+        write(Constants.METHOD_NAME_APPEND, user, user.getId());
+        return user.getId();
     }
 
     @Override
     public boolean deleteUser(long id) {
-        return false;
+        if (getUser(id).getId() == 0) {
+            log.warn(Constants.MESSAGE_NOT_FOUND);
+            return false;
+        }
+        return write(Constants.METHOD_NAME_DELETE, getUser(id), id);
     }
 
     @Override
     public boolean updateUser(User user) {
-        return false;
-    }
-
-    @Override
-    public List<Order> getOrders() {
-        return null;
-    }
-
-    @Override
-    public Order getOrder(long id) {
-        return null;
-    }
-
-    @Override
-    public long insertOrder(Order order) {
-        return 0;
-    }
-
-    @Override
-    public boolean deleteOrder(long id) {
-        return false;
-    }
-
-    @Override
-    public boolean updateOrder(Order order) {
-        return false;
+        long id = user.getId();
+        if (getUser(id).getId() == 0) {
+            log.warn(Constants.MESSAGE_NOT_FOUND);
+            return false;
+        }
+        return write(Constants.METHOD_NAME_UPDATE, user, user.getId());
     }
 
     @Override
     public List<Product> getProducts() {
-        return null;
+        return read(Product.class);
     }
 
     @Override
     public Product getProduct(long id) {
-        return null;
+        List<Product> list = read(Product.class, id);
+        return list.isEmpty() ? new Product() : list.get(0);
     }
 
     @Override
     public long insertProduct(Product product) {
-        return 0;
+        long id = product.getId();
+        if (getProduct(id).getId() != 0)
+            product.setId(System.currentTimeMillis());
+        write(Constants.METHOD_NAME_APPEND, product, product.getId());
+        return product.getId();
     }
 
     @Override
     public boolean deleteProduct(long id) {
-        return false;
+        if (getProduct(id).getId() == 0) {
+            log.warn(Constants.MESSAGE_NOT_FOUND);
+            return false;
+        }
+        return write(Constants.METHOD_NAME_DELETE, getProduct(id), id);
     }
 
     @Override
     public boolean updateProduct(Product product) {
-        return false;
+        long id = product.getId();
+        if (getProduct(id).getId() == 0) {
+            log.warn(Constants.MESSAGE_NOT_FOUND);
+            return false;
+        }
+        return write(Constants.METHOD_NAME_UPDATE, product, product.getId());
     }
 
     @Override
     public List<CreationKit> getCreationKits() {
-        return null;
+        return read(CreationKit.class);
     }
 
     @Override
     public CreationKit getCreationKit(long id) {
-        return null;
+        List<CreationKit> list = read(CreationKit.class, id);
+        return list.isEmpty() ? new CreationKit() : list.get(0);
     }
 
     @Override
     public long insertCreationKit(CreationKit creationKit) {
-        return 0;
+        long id = creationKit.getId();
+        if (getCreationKit(id).getId() != 0)
+            creationKit.setId(System.currentTimeMillis());
+        write(Constants.METHOD_NAME_APPEND, creationKit, creationKit.getId());
+        return creationKit.getId();
     }
 
     @Override
     public boolean deleteCreationKit(long id) {
-        return false;
+        if (getCreationKit(id).getId() == 0) {
+            log.warn(Constants.MESSAGE_NOT_FOUND);
+            return false;
+        }
+        return write(Constants.METHOD_NAME_DELETE, getCreationKit(id), id);
     }
 
     @Override
     public boolean updateCreationKit(CreationKit creationKit) {
-        return false;
+        long id = creationKit.getId();
+        if (getCreationKit(id).getId() == 0) {
+            log.warn(Constants.MESSAGE_NOT_FOUND);
+            return false;
+        }
+        return write(Constants.METHOD_NAME_UPDATE, creationKit, creationKit.getId());
     }
 
     @Override
     public List<EndProduct> getEndProducts() {
-        return null;
+        return read(EndProduct.class);
     }
 
     @Override
     public EndProduct getEndProduct(long id) {
-        return null;
+        List<EndProduct> list = read(EndProduct.class, id);
+        return list.isEmpty() ? new EndProduct() : list.get(0);
     }
 
     @Override
     public long insertEndProduct(EndProduct endProduct) {
-        return 0;
+        long id = endProduct.getId();
+        if (getEndProduct(id).getId() != 0)
+            endProduct.setId(System.currentTimeMillis());
+        write(Constants.METHOD_NAME_APPEND, endProduct, endProduct.getId());
+        return endProduct.getId();
     }
 
     @Override
     public boolean deleteEndProduct(long id) {
-        return false;
+        if (getEndProduct(id).getId() == 0) {
+            log.warn(Constants.MESSAGE_NOT_FOUND);
+            return false;
+        }
+        return write(Constants.METHOD_NAME_DELETE, getEndProduct(id), id);
     }
 
     @Override
     public boolean updateEndProduct(EndProduct endProduct) {
-        return false;
+        long id = endProduct.getId();
+        if (getEndProduct(id).getId() == 0) {
+            log.warn(Constants.MESSAGE_NOT_FOUND);
+            return false;
+        }
+        return write(Constants.METHOD_NAME_UPDATE, endProduct, endProduct.getId());
+    }
+
+    @Override
+    public List<Order> getOrders() {
+        return read(Order.class);
+    }
+
+    @Override
+    public Order getOrder(long id) {
+        List<Order> list = read(Order.class, id);
+        return list.isEmpty() ? new Order() : list.get(0);
+    }
+
+    @Override
+    public long insertOrder(Order order) {
+        long id = order.getId();
+        if (getOrder(id).getId() != 0)
+            order.setId(System.currentTimeMillis());
+        write(Constants.METHOD_NAME_APPEND, order, order.getId());
+        return order.getId();
+    }
+
+    @Override
+    public boolean deleteOrder(long id) {
+        if (getOrder(id).getId() == 0) {
+            log.warn(Constants.MESSAGE_NOT_FOUND);
+            return false;
+        }
+        return write(Constants.METHOD_NAME_DELETE, getOrder(id), id);
+    }
+
+    @Override
+    public boolean updateOrder(Order order) {
+        long id = order.getId();
+        if (getOrder(id).getId() == 0) {
+            log.warn(Constants.MESSAGE_NOT_FOUND);
+            return false;
+        }
+        return write(Constants.METHOD_NAME_UPDATE, order, order.getId());
     }
 }
